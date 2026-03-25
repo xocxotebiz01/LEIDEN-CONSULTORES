7fe8ad6d0dc8cdc9e950a7d3d7dfd4a684da07f5
xocxotebiz01 <guillermorattobiz01@gmail.com>
2026-03-25 11:04:03 -0300

    Update Contact.tsx

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -22,4 +22,16 @@ export const Contact: React.FC = () => {
               <div className="flex items-center gap-4 group">
                 <div className="w-10 h-10 rounded-full bg-white flex items-center justify-center text-slate-900 shadow-sm border border-slate-100">
                   <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
+                    <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0015.003 4H4.997a2 2 0 00-1.994 1.884z"></path>
+                    <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
+                  </svg>
+                </div>
+                <div>
+                  <p className="text-lg font-bold text-slate-900">contacto@leiden.com.ar</p>
+                </div>
+              </div>
+              {/* Teléfono */}
+              <div className="flex items-center gap-4 group">
+                <div className="w-10 h-10 rounded-full bg-white flex items-center justify-center text-slate-900 shadow-sm border border-slate-100">
+                  <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
                    
