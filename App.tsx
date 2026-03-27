31a30612e8d44cdceb9711f1e468c169b0594d73
xocxotebiz01 <guillermorattobiz01@gmail.com>
2026-03-27 19:11:30 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import { Hero } from './components/Hero';
 import { TopTrainings } from './components/TopTrainings';
 import { Services } from './components/Services';
@@ -8,20 +10,38 @@ import { Contact } from './components/Contact';
 import { Footer } from './components/Footer';
 import { Navbar } from './components/Navbar';
 
+// Importá tu página de baja
+import Unsubscribe from './unsubscribe';
+
 const App: React.FC = () => {
   return (
-    <div className="min-h-screen flex flex-col selection:bg-blue-100 selection:text-blue-900">
-      <Navbar />
-      <main className="flex-grow">
-        <Hero />
-        <TopTrainings />
-        <Services />
-        <PainPoints />
-        <ImplementationFocus />
-        <Contact />
-      </main>
-      <Footer />
-    </div>
+    <Router>
+      <div className="min-h-screen flex flex-col selection:bg-blue-100 selection:text-blue-900">
+        <Navbar />
+        <main className="flex-grow">
+          <Routes>
+            {/* Ruta principal */}
+            <Route
+              path="/"
+              element={
+                <>
+                  <Hero />
+                  <TopTrainings />
+                  <Services />
+                  <PainPoints />
+                  <ImplementationFocus />
+                  <Contact />
+                </>
+              }
+            />
+
+            {/* Nueva ruta de baja */}
+            <Route path="/unsubscribe" element={<Unsubscribe />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 };
 
